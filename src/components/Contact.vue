0dc7f706ac1aaaa392f3ecf4bb831367f1131ac3
EDowds98 <ericdowds1998@gmail.com>
2023-02-13 14:23:35 +0000

    linkedin logo update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,13 +4,13 @@
             You can get in touch with me through any of the following methods:
         </h2>
         <div class="grid sm:grid-cols-3 grid-cols-1 mx-auto font-ibm object-center items-center justify-center content-center">
-            <a href="https://www.linkedin.com/in/eric-dowds-10216b1a2/" class="mx-auto my-6">
+            <a href="https://www.linkedin.com/in/eric-dowds-10216b1a2/" class="mx-auto my-6" target="_blank">
                 <img src="../assets/linkedin-logo.jpeg" class="h-52 w-52"/>
             </a>
-            <a href="https://github.com/EDowds98" class="mx-auto my-6">
+            <a href="https://github.com/EDowds98" class="mx-auto my-6" target="_blank">
                 <img src="../assets/github-logo.png" class="h-52 w-52"/>
             </a>
-            <a href="ericdowds@protonmail.com?subject=Enquiry from website" class="mx-auto my-6">
+            <a href="ericdowds@protonmail.com?subject=Enquiry from website" class="mx-auto my-6" target="_blank">
                 <img src="../assets/email-icon.png" class="h-52 w-52"/>
             </a>
         </div>
